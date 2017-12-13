ca2055fd6c7e7008c74ce5ec5ea640749fbc7763
emilyw26 <emilywu1456@gmail.com>
2017-12-12 21:39:32 -0500

    completed exercise 3

--- src/main/java/com/thoughtworks/tw101/exercises/exercise3/OddSum.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise3/OddSum.java
@@ -3,6 +3,12 @@ package com.thoughtworks.tw101.exercises.exercise3;
 public class OddSum {
 
     public int of(int start, int end) {
-        return -1;
+        // This function returns the sum of all all integers between start and end.
+        int sum = 0;
+
+        for (int i = start; i <= end; i += 2) {
+            sum += i;
+        }
+        return sum;
     }
 }
