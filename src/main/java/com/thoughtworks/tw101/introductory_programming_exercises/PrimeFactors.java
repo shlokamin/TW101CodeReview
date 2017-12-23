4e2efc241a8bb386ee5e32aafea67c100947c3fa
emilywu-tw <emilywuthoughtworks@gmail.com>
2017-12-22 23:32:29 -0500

    completed PrimeFactors

--- src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/PrimeFactors.java
+++ src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/PrimeFactors.java
@@ -6,7 +6,7 @@ package com.thoughtworks.tw101.introductory_programming_exercises;
 //
 //  For example, generate(1) should return an empty list and generate(30) should return the numbers: 2,3,5.
 
-import java.util.List;
+import java.util.*;
 
 public class PrimeFactors {
     public static void main(String[] args) {
@@ -14,6 +14,32 @@ public class PrimeFactors {
     }
 
     private static List<Integer> generate(int n) {
-        return null;
+        TreeSet primeFactors = new TreeSet<Integer>();
+
+        // edge case where n = 1 should return an empty list
+        if (n == 1) {
+            return new ArrayList<Integer>();
+        }
+
+        // keep dividing by 2 until the number is no longer even
+        while (n % 2 == 0) {
+            primeFactors.add(2);
+            n = n/2;
+        }
+
+        int loopEnd = (int) Math.round(Math.sqrt(n));
+
+        // now can check the rest of the primes up until the square root of n
+        for (int i = 3; i <= loopEnd + 1; i += 2) {
+            while (n % i == 0) {
+                primeFactors.add(i);
+                n = n/i;
+            }
+        }
+
+        List<Integer> factorsToList = new ArrayList<Integer>(primeFactors);
+
+        return factorsToList;
+
     }
 }
